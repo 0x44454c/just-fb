d2c4e9ebc1e9d8b79ce0e15a43f4c6e17f5b5209
DelusionaL <monimoysaha315@gmail.com>
2021-09-02 11:32:10 +0530

    removed firebase api key

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDLcvhm59AL5KPKaqKTkvISLQU7m8kofLA",
-	authDomain: "just-fb.firebaseapp.com",
-	projectId: "just-fb",
-	storageBucket: "just-fb.appspot.com",
-	messagingSenderId: "995783182541",
-	appId: "1:995783182541:web:36c6b51865fcf575ba8184",
-	measurementId: "G-DKN8QZV5MJ"
+	
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
